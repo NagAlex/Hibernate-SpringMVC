16b1e6c08ae4dece51f1d668d79906d1204c91fe
NagAlex <Alexander.NAG81@gmail.com>
2016-02-07 17:59:58 +0200

    no message

---
+++ SQL/groups_table.sql
@@ -0,0 +1,17 @@
+--
+-- Скрипт сгенерирован Devart dbForge Studio for MySQL, Версия 6.3.341.0
+-- Домашняя страница продукта: http://www.devart.com/ru/dbforge/mysql/studio
+-- Дата скрипта: 28.12.2015 18:10:41
+-- Версия сервера: 5.6.23-log
+-- Версия клиента: 4.1
+--
+
+
+SET NAMES 'utf8';
+
+UPDATE geekdb2.groups_table SET GROUP_NAME = 'FIC' WHERE ID = 1;
+UPDATE geekdb2.groups_table SET GROUP_NAME = 'Other' WHERE ID = 2;
+UPDATE geekdb2.groups_table SET GROUP_NAME = 'NAG' WHERE ID = 5;
+UPDATE geekdb2.groups_table SET GROUP_NAME = 'FromOtherComp' WHERE ID = 24;
+UPDATE geekdb2.groups_table SET GROUP_NAME = 'Group7777777' WHERE ID = 27;
+UPDATE geekdb2.groups_table SET GROUP_NAME = 'NAG777' WHERE ID = 38;
---
+++ SQL/users_table.sql
@@ -0,0 +1,25 @@
+--
+-- Скрипт сгенерирован Devart dbForge Studio for MySQL, Версия 6.3.341.0
+-- Домашняя страница продукта: http://www.devart.com/ru/dbforge/mysql/studio
+-- Дата скрипта: 28.12.2015 18:10:16
+-- Версия сервера: 5.6.23-log
+-- Версия клиента: 4.1
+--
+
+
+SET NAMES 'utf8';
+
+UPDATE geekdb2.users_table SET EMAIL = 'Alexander@gmail.com', FIRST_NAME = 'Alexander', LAST_NAME = 'Nekora', GROUP_ID = 1 WHERE ID = 1;
+UPDATE geekdb2.users_table SET EMAIL = 'nina.korinna@fcbank.com.ua', FIRST_NAME = 'Nina', LAST_NAME = 'Korinna', GROUP_ID = 1 WHERE ID = 2;
+UPDATE geekdb2.users_table SET EMAIL = 'valentyna.yeremenko@fcbank.com.ua', FIRST_NAME = 'Valentina', LAST_NAME = 'Yeremenko', GROUP_ID = 1 WHERE ID = 3;
+UPDATE geekdb2.users_table SET EMAIL = 'serhii.tarasenko@fcbank.com.ua', FIRST_NAME = 'Serhiy', LAST_NAME = 'Tarasenko', GROUP_ID = 2 WHERE ID = 4;
+UPDATE geekdb2.users_table SET EMAIL = 'Ramail333@ukr.net', FIRST_NAME = 'Tolik', LAST_NAME = 'Rudenko', GROUP_ID = 2 WHERE ID = 5;
+UPDATE geekdb2.users_table SET EMAIL = 'alex@alex.com', FIRST_NAME = 'Alex', LAST_NAME = 'Nag', GROUP_ID = 5 WHERE ID = 8;
+UPDATE geekdb2.users_table SET EMAIL = 'Natalia.Borkun@fcbank.com.ua', FIRST_NAME = 'Natalia', LAST_NAME = 'Borkun', GROUP_ID = 24 WHERE ID = 13;
+UPDATE geekdb2.users_table SET EMAIL = 'Yulija.Vasilenko@cherkassy.fcbank.com.ua', FIRST_NAME = 'Julija', LAST_NAME = 'Vasilenko', GROUP_ID = 24 WHERE ID = 14;
+UPDATE geekdb2.users_table SET EMAIL = 'Nataliya.Volkova@cherkasy.fcbank.com.ua', FIRST_NAME = 'Nataliya', LAST_NAME = 'Volkova', GROUP_ID = 27 WHERE ID = 15;
+UPDATE geekdb2.users_table SET EMAIL = 'Maryna.Dobrodiieva@cherkasy.fcbank.com.ua', FIRST_NAME = 'Maryna', LAST_NAME = 'Dobrodiieva', GROUP_ID = 27 WHERE ID = 16;
+UPDATE geekdb2.users_table SET EMAIL = 'Irina.Zaika@fcbank.com.ua', FIRST_NAME = 'Irina', LAST_NAME = 'Zaika', GROUP_ID = 2 WHERE ID = 17;
+UPDATE geekdb2.users_table SET EMAIL = 'Mykola.Koval@fcbank.com.ua', FIRST_NAME = 'Mykola', LAST_NAME = 'Koval', GROUP_ID = 27 WHERE ID = 18;
+UPDATE geekdb2.users_table SET EMAIL = 'archuk@gmail.com', FIRST_NAME = 'Artem', LAST_NAME = 'Darchukus', GROUP_ID = 2 WHERE ID = 21;
+UPDATE geekdb2.users_table SET EMAIL = 'alexius@alex.com', FIRST_NAME = 'Alexius', LAST_NAME = 'Nagius', GROUP_ID = 5 WHERE ID = 22;
